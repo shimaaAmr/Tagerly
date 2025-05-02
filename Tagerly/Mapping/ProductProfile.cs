b71e2937770a01cbf167019f5de5bace8ba0ab80
nouryounis666 <nouryounis666@gmail.com>
2025-05-02 20:08:00 +0300

    Update product profile

--- Tagerly/Mapping/ProductProfile.cs
+++ Tagerly/Mapping/ProductProfile.cs
@@ -8,23 +8,22 @@ namespace Tagerly.Mapping
         public ProductProfile()
         {
             CreateMap<Product, ProductViewModel>()
-                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Id)) // Explicit ID mapping
+                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name))
                 .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
-                ;  
+                ;
 
             CreateMap<ProductViewModel, Product>()
-                .ForMember(dest => dest.Category, opt => opt.Ignore()) 
+                .ForMember(dest => dest.Category, opt => opt.Ignore())
                 .ForMember(dest => dest.ImageUrl, opt => opt.Ignore())
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ProductId));// Reverse mapping;  
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ProductId));
 
-            // تحويل بين Category و CategoryViewModel
             CreateMap<Category, CategoryViewModel>()
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))  
-                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));  
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
 
             CreateMap<CategoryViewModel, Category>()
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))  
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
         }
     }
