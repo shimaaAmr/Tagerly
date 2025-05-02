1fe8b048c250f93b0ab701177b05dbb4e26b046a
Haneen-Elagamy <haneenelagamy21@gmail.com>
2025-05-02 04:12:31 +0300

    Add  4 new View Models

---
+++ Tagerly/ViewModels/ProductPagedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Tagerly.ViewModels
+{
+    public class ProductPagedResult
+    {
+        public IEnumerable<ProductViewModel> Products { get; set; }
+        public int TotalCount { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    }
+}
---
+++ Tagerly/ViewModels/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Tagerly.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
