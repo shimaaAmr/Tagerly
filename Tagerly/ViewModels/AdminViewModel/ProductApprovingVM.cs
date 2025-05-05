35f8aeef99164811cff6cc47efb29466dd23336e
Mahmoud-Fathi23 <mahmoudfathymf576041@gmail.com>
2025-05-06 01:23:51 +0300

    Add new change view aprved

--- Tagerly/ViewModels/AdminViewModel/ProductApprovingVM.cs
+++ Tagerly/ViewModels/AdminViewModel/ProductApprovingVM.cs
@@ -9,6 +9,9 @@ namespace Tagerly.ViewModels.AdminViewModel
         public string Description { get; set; }
         public string SellerName { get; set; }
         public string ImageUrl { get; set; }
+        public bool? IsApproved { get; set; } = null;       // null = لسه، true = موافق، false = مرفوض
+        public bool IsReject { get; set; } = false;        // للحذف المنطقي
+
         public ProductStatus Status { get; set; }
     }
 }
