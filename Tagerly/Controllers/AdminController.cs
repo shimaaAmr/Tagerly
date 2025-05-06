0e47b242627741f65ddd103cf3e4e2023228252d
Haneen-Elagamy <haneenelagamy21@gmail.com>
2025-05-06 20:11:31 +0300

    Add Basic Dashboard

---
+++ Tagerly/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tagerly.Services.Interfaces.Admin;
+using Tagerly.ViewModels.AdminViewModel;
+
+namespace Tagerly.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IDashboardService _dashboardService;
+
+        public AdminController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            var model = new DashboardViewModel
+            {
+                TotalUsers = await _dashboardService.GetTotalUsersAsync(),
+                TotalProducts = await _dashboardService.GetTotalProductsAsync(),
+                TotalOrders = await _dashboardService.GetTotalOrdersAsync(),
+                TotalRevenue = await _dashboardService.GetTotalRevenueAsync()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Tagerly/Services/Implementations/Admin/DashboardService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tagerly.DataAccess.DbContexts;
+using Tagerly.Services.Interfaces.Admin;
+
+namespace Tagerly.Services.Implementations.Admin
+{
+    public class DashboardService:IDashboardService
+    {
+        private readonly TagerlyDbContext _context;
+
+        public DashboardService(TagerlyDbContext context)
+        {
+           _context = context;
+        }
+
+        public async Task<int> GetTotalUsersAsync()
+        {
+            return await _context.Users.CountAsync();
+        }
+
+        public async Task<int> GetTotalProductsAsync()
+        {
+            return await _context.Products.CountAsync();
+        }
+
+        public async Task<int> GetTotalOrdersAsync()
+        {
+            return await _context.Orders.CountAsync();
+        }
+
+        public async Task<decimal> GetTotalRevenueAsync()
+        {
+            return await _context.OrderDetails
+                .SumAsync(od => od.Price * od.Quantity);
+        }
+    }
+}
